206e588bfd05dfa875f892ac8811e1d50ad64483
alula-z <azeruesenay14@gmail.com>
2024-12-31 23:10:11 -0500

    Implemented Stat Scroll view front end

--- anime-tracker/screens/ProfileScreen.js
+++ anime-tracker/screens/ProfileScreen.js
@@ -1,48 +1,128 @@
 import React from "react";
-import {ImageBackground, StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import {
+  ScrollView,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
-
-const ProfileScreen = () =>{
-    return (
-        <View>
-        <View >
+const ProfileScreen = () => {
+  return (
+    <View>
+      <View>
         <ImageBackground
-                source = {require('../assets/Images/TestLogo.png')}
-                style = {styles.backgroundImg }>
-            <View style = {styles.container}>
-            <Text style = {styles.profileText}> Avatar placeholder</Text>
-            <Text style = {styles.profileText}> Username</Text>
-            <Text style = {styles.profileText}>Experience Level</Text>
-            </View>
+          source={require("../assets/Images/TestLogo.png")}
+          style={styles.backgroundImg}
+        >
+          <View style={styles.container}>
+            <Text style={styles.profileText}> Avatar placeholder</Text>
+            <Text style={styles.profileText}> Username</Text>
+            <Text style={styles.profileText}>Experience Level</Text>
+          </View>
         </ImageBackground>
+      </View>
+      <View style={StyleSheet.container}>
+        {/*Stats section*/}
+        <View style={styles.category}>
+          <Text style={styles.categoryText}>Stats</Text>
         </View>
-        <View style = {StyleSheet.container}>
-
-            <Text> Profile</Text>
-        </View>
-        
-        </View>
-    );
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator = {false}>
+          {/* currently watching, completed, favorites, time Watched */}
+          <View style={styles.container}>
+            <View style={styles.stat}>
+              <Text style={styles.statCat}>Completed </Text>
+            </View>
+            <View style = {styles.statNumBox}>
+            <Text style={styles.statNum}>00</Text>
+            </View>
+          </View>
+          <View style={styles.container}>
+            <View style={styles.stat}>
+              <Text style={styles.statCat}>Favorites</Text>
+            </View>
+            <View style = {styles.statNumBox}>
+                <Text style={styles.statNum}>00</Text>
+            </View>
+          </View>
+          <View style={styles.container}>
+            <View style={styles.stat}>
+              <Text style={styles.statCat}>Time Watched</Text>
+            </View>
+            <View style = {styles.statNumBox}>
+              <Text style={styles.statNum}>01Y 00M 00d</Text>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1 ,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-  
-    backgroundImg:{
-        resizeMode: 'cover',
-        height: 150,
-        opacity: 0.9,
-    },
-    profileText:{
-        color: 'red',
-        fontSize: 30,
-        fontWeight: 'bold',
-        fontStyle: 'italic',
-    },
+  horizontal: {
+    display: "flex",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 3,
+  },
+
+  backgroundImg: {
+    resizeMode: "cover",
+    height: 150,
+    opacity: 0.9,
+    borderColor: "black",
+  },
+  category: {
+    borderColor: "black",
+    borderWidth: 2,
+    paddingLeft: 8,
+    height: 50,
+    justifyContent: "center",
+  },
+  categoryText: {
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  profileText: {
+    color: "red",
+    fontSize: 30,
+    fontWeight: "bold",
+    fontStyle: "italic",
+  },
+  stat: {
+    borderColor: "black",
+    borderWidth: 2,
+    width: 150,
+    height: 50,
+    marginTop: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  statCat: {
+    fontSize: 15,
+    fontWeight: "bold",
+    paddingLeft: 3,
+    paddingRight: 3,
+  },
+  statNum: {
+    fontSize: 20,
+    borderTopWidth: 0,
+  },
+  statNumBox: {
+    borderColor: "black",
+    borderWidth: 2,
+    borderTopWidth: 0,
+    width: 150,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  }
 });
 
 export default ProfileScreen;
