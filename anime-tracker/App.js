ad67814d89e570dcbae37759471a233c64ee9f06
alula-z <azeruesenay14@gmail.com>
2025-01-04 14:55:10 -0500

    Began userRegistration

--- anime-tracker/App.js
+++ anime-tracker/App.js
@@ -3,23 +3,44 @@ import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { NavigationContainer } from '@react-navigation/native';
 import Navigator from './navigation/Navigator';
-
+import { UserRegistration } from './screens/UserRegistration';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <NavigationContainer>
-    <View style = {styles.navBar}>
-      <Navigator/>
-      </View>
-    </NavigationContainer>
+    <>
+    <StatusBar/>
+    <SafeAreaView style = {styles.container}>
+    <>
+    <Text style = {styles.title}> Sign Up</Text>
+    <UserRegistration/>
+    </>
+    </SafeAreaView>
+    </>
   );
 }
+{/*<NavigationContainer>
+      
+      <View style = {styles.navBar}>
+        <Navigator/>
+        </View>
+      </NavigationContainer> */}
 
 const styles = StyleSheet.create({
   navBar: {
     flex: 1,
     bottom: 0,
     
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent:'center',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 25,
   },
   
 });
--- anime-tracker/screens/UserRegistration.js
+++ anime-tracker/screens/UserRegistration.js
@@ -3,17 +3,61 @@ import { Button, StyleSheet, TextInput } from "react-native";
 import Parse from "parse/react-native";
 
 
-export const UserRegistration = () =>{
+export const UserRegistration = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const doUserRegistration = async function(){
+    try{
+    const usernameVal = username.trim();
+    const passwordVal = password;
 
-    return(
-        <>
-        </>
-    )
-}
+    const createdUser =  await Parse.User.signUp(usernameVal,passwordVal)
+        Alert.alert(
+            "Sucessful",
+            'User ${createdUser.get("username")} was succesfull'
+        );
+    } catch(error) {
+        Alert.alert("Error", error.message);
+        
+    }
+  };
+  return (
+    <>
+      <TextInput
+        style={styles.input}
+        value={username}
+        autoCapitalize={"none"}
+        placeholder={"Username"}
+        onChangeText={(text) => setUsername(text)}
+      />
+      <TextInput
+        style={styles.input}
+        value={password}
+        autoCapitalize={"none"}
+        textContentType={"Password"}
+        placeholder={"Password"}
+        secureTextEntry
+        onChangeText={(text) => setPassword(text)}
+      />
+
+      <Button  title = {"Sign Up"} onPress={() => doUserRegistration()}/>
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
+  input: {
+    backgroundColor: '#efefef',
+    height: '35',
+    marginBottom: 10,
+    width: '300',
+    height: '40',
+    borderWidth: 2,
+    padding: 3,
+  },
+  signUpButton:{
+    backgroundColor: 'red',
+  },
 
-    input:{
-
-    },
-})
+});
